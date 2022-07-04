ccfa463fbd00f0b1e47179f8dcdf53b310715b5e
Maksim Bezdrobnij <maksim.bezdrobnij@wallester.com>
2022-07-04 16:10:21 +0300

    Initial Commit

---
+++ MaterialColorPalett/MaterialColorPalettApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialColorPalettApp.swift
+//  MaterialColorPalett
+//
+//  Created by Maksim Bezdrobnoi on 04.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MaterialColorPalettApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
